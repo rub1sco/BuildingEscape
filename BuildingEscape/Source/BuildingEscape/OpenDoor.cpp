c1fb8affc9c2bbd25c672f395dfb34ac82d78299
Tom <tom.lucero@me.com>
2018-11-17 19:58:50 -0700

    Master Branch

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+    Owner = GetOwner();
+    ActorThatOpens = GetWorld()-> GetFirstPlayerController()-> GetPawn();
+    
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    //Poll trigger Volume at every frame
+    if (PressurePlate -> IsOverlappingActor(ActorThatOpens)){
+        OpenDoor();
+        DoorLastOpenTime = GetWorld()->GetTimeSeconds();
+    }
+    
+    //TODO Check if time to close door
+    if(GetWorld()-> GetTimeSeconds() - DoorLastOpenTime > DoorCloseDelay){
+        CloseDoor();
+    }
+    
+    
+	// ...
+}
+
+void UOpenDoor::OpenDoor(){
+    Owner -> SetActorRotation(FRotator(0.0f, OpenAngle,0.0f));
+    
+}
+
+void UOpenDoor::CloseDoor(){
+    Owner -> SetActorRotation(FRotator(0.0f, 90.0f,0.0f));
+}
---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
+#include "OpenDoor.generated.h"
+
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+    
+    void OpenDoor();
+    void CloseDoor();
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+    
+private:
+    UPROPERTY(EditAnywhere)
+    float OpenAngle = 0.0f;
+    
+	UPROPERTY(EditAnywhere)
+    ATriggerVolume* PressurePlate;
+    
+    UPROPERTY(EditAnywhere)
+    float DoorCloseDelay = 1.f;
+    float DoorLastOpenTime;
+    
+    //GetWorld method in CPP replaces this in editable area in gameplay
+//    UPROPERTY(EditAnywhere)
+    AActor* ActorThatOpens; //Remember Pawn inherits from actor
+    AActor* Owner; //The owning door
+    
+    
+	
+};
